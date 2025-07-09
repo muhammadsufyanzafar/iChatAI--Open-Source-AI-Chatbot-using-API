c4a4be3c5059581b5326f76056ed83f348c7c13c
Muhammad Sufyan Zafar <99420706+muhammadsufyanzafar@users.noreply.github.com>
2025-07-09 23:11:50 +0500

    Updated

--- app/src/main/java/com/zafar/ichatai/BuildConfig.java
+++ app/src/main/java/com/zafar/ichatai/BuildConfig.java
@@ -1,5 +1,7 @@
 package com.zafar.ichatai;
 
 public class BuildConfig {
+                                       //    Your API key will land here
     public static String apiKey = "AIzaSyB2rl0ES0pflUicM5z2U5TEQ3eiSzE18RE";
+
 }
